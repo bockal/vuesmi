61337dfb7b3e0fe613d21bb4a1e4d3d10d757d76
Aubrey <bockal@gmail.com>
2026-08-29 15:26:01 -0400

    Use the Vues bell mark in the web app manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,6 +9,6 @@ export default function manifest():MetadataRoute.Manifest{
     display:"standalone",
     background_color:"#ffffff",
     theme_color:"#173f3a",
-    icons:[{src:"/favicon.svg",sizes:"any",type:"image/svg+xml",purpose:"any"}],
+    icons:[{src:"/vues-bell.svg",sizes:"any",type:"image/svg+xml",purpose:"any"}],
   };
 }
