d832bcc6afa4ddfd452a92113bc8d26d5603bd98
Aubrey <bockal@gmail.com>
2026-08-30 17:26:50 -0400

    Add crawlable sitemap and robots routes

---
+++ app/robots.txt/route.ts
@@ -0,0 +1,12 @@
+export async function GET(){
+  const text=`User-agent: *
+Allow: /
+Disallow: /owner/
+Disallow: /api/
+
+Sitemap: https://vuesmi.com/sitemap.xml
+Host: https://vuesmi.com
+`;
+  return new Response(text,{headers:{"content-type":"text/plain; charset=utf-8","cache-control":"public, max-age=3600"}});
+}
+
