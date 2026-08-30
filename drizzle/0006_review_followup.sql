3aa6ba962a5902166279cef02fe9df6beedfde4b
Aubrey <bockal@gmail.com>
2026-08-30 19:34:01 -0400

    Add analytics and review follow-up support

---
+++ drizzle/0006_review_followup.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `booking_requests` ADD `review_sent_at` text;
+--> statement-breakpoint
+UPDATE `booking_requests` SET `review_sent_at` = CURRENT_TIMESTAMP WHERE `departure` < date('now');
+
